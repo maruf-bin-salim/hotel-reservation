ae0d0e7768c42b5ca148f31c61f0d7fc156ae0ed
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-06-21 00:49:20 +0600

    added structure to home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,68 @@
 import AuthUI from "@/components/AuthUI/AuthUI"
+import styles from '@/styles/index.module.css'
 
-function Index() {
+function Hotel({ hotel }) {
   return (
-    <div>
-      <h1>Next.js API</h1>
-      <p>API version: beta 0.0.1</p>
+    <div className={styles.hotel}>
+      <div className={styles.hotel_image}>
+        <img src="https://cdn.lapaninja.com/assets/images/airbnb-08-06-2021-thumb.jpg" />
+      </div>
+
+      <div className={styles.hotel_name}>
+        Hotel Name
+      </div>
+
+      <div className={styles.hotel_address}>
+        Hotel Address
+      </div>
+
+      <div className={styles.hotel_rating}>
+        <div className={styles.hotel_rating_stars}>
+        </div>
+      </div>
+        <button className={styles.hotel_details_button}>
+          view hotel details
+        </button>
+    </div>
+  )
+}
+
+
+function Index({ user }) {
+  return (
+    <div className={styles.page}>
+
+      {/* top bar */}
+      <div className={styles.top_bar}>
+        <div className={styles.top_bar_left}>
+          <div className={styles.logo} />
+        </div>
+
+        <div className={styles.top_search_bar}>
+          <input type="text" placeholder="Search by city name or adress" />
+          <div className={styles.search_icon} />
+        </div>
+
+        <div className={styles.top_bar_right}>
+          <div className={styles.user_name}>
+            {user ? user.email.split('@')[0] : ''}
+          </div>
+          <div className={styles.logout_button} />
+        </div>
+      </div>
+
+      {/* main content */}
+      <div className={styles.main_content}>
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+        <Hotel />
+      </div>
+
     </div>
   )
 }
