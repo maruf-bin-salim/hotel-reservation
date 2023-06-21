0e18d8b2ea03f840ffa2387c5ffd0e1e40986f61
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-06-21 20:05:40 +0600

    updated

--- pages/hotel/[id].js
+++ pages/hotel/[id].js
@@ -2,12 +2,13 @@ import AuthUI from '@/components/AuthUI/AuthUI'
 import { getHotelByIdfromDatabase } from '@/database/functions';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
+import styles from '@/styles/hotel.module.css'
 
 function Hotel({ user }) {
 
     const [hotel, setHotel] = useState(null);
     const router = useRouter();
-    
+
     async function fetchedHotel(id) {
         const hotel = await getHotelByIdfromDatabase(id);
         setHotel(hotel);
@@ -18,11 +19,55 @@ function Hotel({ user }) {
             fetchedHotel(router.query.id);
         }
     }, [router.query.id])
-        
+
+
+    if (!hotel) {
+        return (
+            <div className={styles.loading}>
+                Loading...
+            </div>
+        )
+    }
+
     return (
-        <div>
-            HI {user.email}
-            HOTEL {JSON.stringify(hotel)}
+        <div className={styles.page}>
+            <div className={styles.cover}
+                style={{
+                    backgroundImage: `${hotel.image && hotel.image !== '' ? `url(${hotel.image})` : `url(/default.png)`}`,
+                }}
+            >
+                <div className={styles.back_button} onClick={() => { router.push('/') }} />
+                <div className={styles.main}>
+
+                    <h1 className={styles.name}>
+                        {hotel.name}
+                    </h1>
+
+                    <div className={styles.address}>
+                        {hotel.address}
+                    </div>
+                    <div className={styles.description}>
+                        {hotel.description}
+                    </div>
+
+                    {
+                        (Math.floor(hotel.rating) > 0) ?
+                            <div className={styles.rating_stars}>
+                                {`${hotel.rating} / 5`}
+                            </div>
+
+                            :
+                            <div className={styles.rating_stars}>
+                                {`No rating yet!`}
+                            </div>
+                    }
+
+                </div>
+            </div>
+
+            {/* main */}
+            <div className={styles.main_content}>
+            </div>
         </div>
     )
 }
